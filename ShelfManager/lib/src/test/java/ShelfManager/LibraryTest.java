cbd5d65cc76585e9376d6f87fec139f48d7eb7bd
Markus Wagner <markus.wagner_1@student.hs-rm.de>
2021-05-03 15:46:33 +0200

    Testcommit von Markus

--- ShelfManager/lib/src/test/java/ShelfManager/LibraryTest.java
+++ ShelfManager/lib/src/test/java/ShelfManager/LibraryTest.java
@@ -10,5 +10,6 @@ public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+        System.out.println("Gruﬂ von Markus");
     }
 }
