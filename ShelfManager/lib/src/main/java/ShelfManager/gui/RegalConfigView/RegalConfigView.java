0ff90ba4ecd10b222dfe9b93983dd5eac1af16c4
Markus Wagner <markus.wagner_1@student.hs-rm.de>
2021-06-28 09:12:05 +0200

    added backToLagerView-Button to RegalConfigView and RegalConfigViewController

--- ShelfManager/lib/src/main/java/ShelfManager/gui/RegalConfigView/RegalConfigView.java
+++ ShelfManager/lib/src/main/java/ShelfManager/gui/RegalConfigView/RegalConfigView.java
@@ -1,14 +1,24 @@
 package ShelfManager.gui.RegalConfigView;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 
 public class RegalConfigView extends BorderPane {
+
+    private Button backToLagerView;
 
     public RegalConfigView() {
 
         Label viewName = new Label("RegalConfigView");
         this.setTop(viewName);
 
+        backToLagerView = new Button("zurueck");
+        this.setBottom(backToLagerView);
+
+    }
+
+    public Button getBackToLagerView() {
+        return backToLagerView;
     }
 }
--- ShelfManager/lib/src/main/java/ShelfManager/gui/RegalConfigView/RegalConfigViewController.java
+++ ShelfManager/lib/src/main/java/ShelfManager/gui/RegalConfigView/RegalConfigViewController.java
@@ -2,7 +2,10 @@ package ShelfManager.gui.RegalConfigView;
 
 import ShelfManager.Lager.Lager;
 import ShelfManager.ShelfManagerApplication;
+import ShelfManager.gui.Scenes;
 import ShelfManager.gui.ViewController;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 
 public class RegalConfigViewController extends ViewController {
@@ -11,11 +14,14 @@ public class RegalConfigViewController extends ViewController {
     private ShelfManagerApplication main;
     private RegalConfigView regalConfigView;
 
+    private Button backToLagerView;
+
     public RegalConfigViewController(Lager hauptLager, ShelfManagerApplication main) {
         this.hauptLager = hauptLager;
         this.main = main;
         this.regalConfigView = new RegalConfigView();
 
+        this.backToLagerView = regalConfigView.getBackToLagerView();
 
         rootView = this.regalConfigView;
         initialize();
@@ -24,7 +30,9 @@ public class RegalConfigViewController extends ViewController {
 
     @Override
     public void initialize() {
-
+        backToLagerView.addEventHandler(ActionEvent.ACTION, event -> {
+            main.switchScene(Scenes.LAGER_VIEW);
+        });
     }
 
     public Pane getRootView() {
