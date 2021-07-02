bf2fe8eb7a2b41e5f851523d81e00db6d3c4c595
Markus Wagner <markus.wagner_1@student.hs-rm.de>
2021-07-02 12:58:28 +0200

    created LagerComponent and LagerComponentController  -> added to LagerViewController
    show RegalComponents in LagerView

---
+++ ShelfManager/lib/src/main/java/ShelfManager/gui/LagerView/LagerComponent/LagerComponent.java
@@ -0,0 +1,12 @@
+package ShelfManager.gui.LagerView.LagerComponent;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
+
+public class LagerComponent extends Pane {
+
+    public LagerComponent() {
+        this.setStyle("-fx-background-color: rgba(204, 204, 204, 1)");
+    }
+}
--- ShelfManager/lib/src/main/java/ShelfManager/gui/LagerView/LagerViewController.java
+++ ShelfManager/lib/src/main/java/ShelfManager/gui/LagerView/LagerViewController.java
@@ -3,7 +3,10 @@ package ShelfManager.gui.LagerView;
 import ShelfManager.Lager.Lager;
 import ShelfManager.Lager.Paket;
 import ShelfManager.Lager.Regal;
+import ShelfManager.Lager.Stuetze;
 import ShelfManager.ShelfManagerApplication;
+import ShelfManager.gui.LagerView.LagerComponent.LagerComponent;
+import ShelfManager.gui.LagerView.LagerComponent.LagerComponentController;
 import ShelfManager.gui.RegalComponent.RegalComponent;
 import ShelfManager.gui.RegalComponent.RegalComponentController;
 import ShelfManager.gui.Scenes;
@@ -16,6 +19,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
 import javafx.util.Callback;
 
 import java.util.ArrayList;
@@ -30,6 +34,9 @@ public class LagerViewController extends ViewController {
     private Button lageruebersichtButton;
     private ListView<Paket> createdPaketsListView;
 
+    private LagerComponentController lagerComponentController;
+    private LagerComponent lagerComponent;
+
     public LagerViewController(Lager hauptLager, ShelfManagerApplication main) {
         this.main = main;
         this.hauptLager = hauptLager;
@@ -38,6 +45,10 @@ public class LagerViewController extends ViewController {
         this.createPacketbutton = lagerView.getCreatePacketButton();
         this.lageruebersichtButton = lagerView.getLageruebersichtButton();
         this.createdPaketsListView = lagerView.getCreatedPaketsListView();
+
+        this.lagerComponentController = new LagerComponentController(hauptLager);
+        this.lagerComponent = (LagerComponent) lagerComponentController.getRootView();
+        lagerView.setCenter(lagerComponent);
 
         rootView = this.lagerView;
         initialize();
@@ -59,39 +70,39 @@ public class LagerViewController extends ViewController {
 
     }
 
-        /*createdPaketsListView.setCellFactory(new Callback<ListView<Paket>, ListCell<Paket>>() {
-            @Override
-            public ListCell<Paket> call(ListView<Paket> param) {
-                return new PaketCell();
-            }
-        });
+//        createdPaketsListView.setCellFactory(new Callback<ListView<Paket>, ListCell<Paket>>() {
+//            @Override
+//            public ListCell<Paket> call(ListView<Paket> param) {
+//                return new PaketCell();
+//            }
+//        });
 
-        ObservableList<Paket> uiModel = createdPaketsListView.getItems();
-        ObservableList<Paket> pakete = hauptLager.getObservablePaketList();
-        uiModel.addAll(pakete);
+//        ObservableList<Paket> uiModel = createdPaketsListView.getItems();
+//        ObservableList<Paket> pakete = hauptLager.getObservablePaketList();
+//        uiModel.addAll(pakete);
+//
+//        pakete.addListener((ListChangeListener<Paket>) change -> {
+//            System.out.println(change);
+//            uiModel.clear();
+//            uiModel.addAll(pakete);
+//            createdPaketsListView.refresh();
+//        });
 
-        pakete.addListener((ListChangeListener<Paket>) change -> {
-            System.out.println(change);
-            uiModel.clear();
-            uiModel.addAll(pakete);
-            createdPaketsListView.refresh();
-        });
+//        ObservableList<Regal> regale = hauptLager.getObservableRegalList();
 
-        ObservableList<Regal> regale = hauptLager.getObservableRegalList();
-
-        ObservableList<Node> regaleInLager = lagerView.getCenterPane().getChildren();
-        regale.addListener(new ListChangeListener<Regal>() {
-            @Override
-            public void onChanged(Change<? extends Regal> c) {
-                for (Regal r : regale) {
-                    System.out.println("Changed");
-                    RegalComponentController regalComponentController = new RegalComponentController(r);
-                    RegalComponent regalComponent = (RegalComponent) regalComponentController.getRootView();
-                    regaleInLager.add(regalComponent);
-                }
-            }
-        });
-    }*/
+//        ObservableList<Node> regaleInLager = lagerView.getCenterPane().getChildren();
+//        regale.addListener(new ListChangeListener<Regal>() {
+//            @Override
+//            public void onChanged(Change<? extends Regal> c) {
+//                for (Regal r : regale) {
+//                    System.out.println("Changed");
+//                    RegalComponentController regalComponentController = new RegalComponentController(r);
+//                    RegalComponent regalComponent = (RegalComponent) regalComponentController.getRootView();
+//                    regaleInLager.add(regalComponent);
+//                }
+//            }
+//        });
+//    }
 
     public Pane getRootView() {
         return rootView;
